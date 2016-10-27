13d13baf4438a2075f9a10136f30c551a631bf2b
Evan <daley.evan@gmail.com>
2016-10-26 18:58:23 -0700

    Add basic input manager

--- New Unity Project/Assets/Scripts/ControllerInput.cs
+++ New Unity Project/Assets/Scripts/ControllerInput.cs
@@ -14,9 +14,28 @@ public class ControllerInput : MonoBehaviour {
 	
 	void Update () 
 	{
-		if (Input.GetAxis (""))
+		if (Input.GetButtonDown("A_1"))
 		{
+			inputValue = "a button";
+			textObject.text = inputValue;
+		}
 
+		if (Input.GetButtonDown("B_1"))
+		{
+			inputValue = "b button";
+			textObject.text = inputValue;
+		}
+
+		if (Input.GetButtonDown("X_1"))
+		{
+			inputValue = "x button";
+			textObject.text = inputValue;
+		}
+
+		if (Input.GetButtonDown("Y_1"))
+		{
+			inputValue = "y button";
+			textObject.text = inputValue;
 		}
 	}
 }
