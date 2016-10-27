b434c25b4f84c1bbf6929e4064f56a45ba35434b
Evan <daley.evan@gmail.com>
2016-10-26 18:45:41 -0700

    Change meta data to plane text

---
+++ New Unity Project/Assets/Scripts/ControllerInput.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ControllerInput : MonoBehaviour {
+
+	public string inputValue = "Please connect controller and press button";
+	public Text textObject;
+
+	void Start ()
+	{
+		textObject.text = inputValue;
+	}
+	
+	void Update () 
+	{
+		if (Input.GetAxis (""))
+		{
+
+		}
+	}
+}
